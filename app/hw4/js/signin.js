6d334087feee03b584fe39fc499fb214d190fb9c
Liam Kelly <likelly@ucsd.edu>
2017-11-25 13:29:37 -0800

    minor html changes, games can now be added

--- app/hw4/js/signin.js
+++ app/hw4/js/signin.js
@@ -4,8 +4,8 @@ function loginUser() {
     let name = document.getElementById('name');
     let pass = document.getElementById('password');
     let user = JSON.parse(localStorage.getItem('users'))[name.value];
-    if((name.value === "manager" && pass.value === "mrmanager") ||
-       (user && user.password == pass.value)){
+    console.log(user);
+    if(user && user.password == pass.value){
         localStorage.setItem("currUser", name.value);
         window.location.replace("./Manage.html");
     }
