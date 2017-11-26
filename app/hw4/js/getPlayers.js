1bb6c71de9a92cc8bc786603d38a9d7f59497be6
koumorikun <turbohuynh@gmail.com>
2017-11-25 19:28:21 -0800

    getplaayers

---
+++ app/hw4/js/getPlayers.js
@@ -0,0 +1,15 @@
+'use strict'
+function getPlayers(){
+  // let players = document.getElementById('players');//empty div to inject templates
+  let roster = localStorage.getItem('roster') ? JSON.parse(localStorage.getItem('roster')) : [];
+  roster.forEach(function(player){
+    let playerTemplate = document.getElementById('player').content.cloneNode(true);//template
+    playerTemplate.querySelector('#name').textContent = player.name;
+    playerTemplate.querySelector('#position').textContent = player.position;
+    playerTemplate.querySelector('#jnumber').textContent = player.jnumber;
+    playerTemplate.querySelector('#birthday').textContent = player.birthday;
+    document.body.appendChild(playerTemplate);
+  });
+}
+
+window.onload = () => {getPlayers()};
