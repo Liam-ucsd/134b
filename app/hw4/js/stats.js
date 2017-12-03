dd0f05d3aa7ad893945e8fcd333b622da46441e7
koumorikun <turbohuynh@gmail.com>
2017-12-02 19:16:00 -0800

    read player stats functional

--- app/hw4/js/stats.js
+++ app/hw4/js/stats.js
@@ -11,70 +11,91 @@ var config = {
 };
 firebase.initializeApp(config);
 
-let playerStats = [
-  {
-    jnumber : 1,
-    name : 'John Smith',
-    foul : 0,
-    redCard : 1,
-    yellowCard : 1,
-    shotsOnGoal : 1,
-    goals : 0,
-    cornerKicks : 0,
-    goalKicks : 1,
-    penaltyKicks : 1,
-    throwIns : 0,
-    appearances : 1
-  },
-  {
-    jnumber : 22,
-    name : 'Jonny Smitten',
-    foul : 0,
-    redCard : 1,
-    yellowCard : 1,
-    shotsOnGoal : 1,
-    goals : 0,
-    cornerKicks : 0,
-    goalKicks : 1,
-    penaltyKicks : 1,
-    throwIns : 0,
-    appearances : 1
-  },
-  {
-    jnumber : 101,
-    name : 'Yohann Smot',
-    foul : 0,
-    redCard : 1,
-    yellowCard : 1,
-    shotsOnGoal : 1,
-    goals : 0,
-    cornerKicks : 0,
-    goalKicks : 1,
-    penaltyKicks : 1,
-    throwIns : 0,
-    appearances : 1
-  }
-];
+// let playerStats = [
+//   {
+//     jnumber : 1,
+//     name : 'John Smith',
+//     foul : 0,
+//     redCard : 1,
+//     yellowCard : 1,
+//     shotsOnGoal : 1,
+//     goals : 0,
+//     cornerKicks : 0,
+//     goalKicks : 1,
+//     penaltyKicks : 1,
+//     throwIns : 0,
+//     appearances : 1
+//   },
+//   {
+//     jnumber : 22,
+//     name : 'Jonny Smitten',
+//     foul : 0,
+//     redCard : 1,
+//     yellowCard : 1,
+//     shotsOnGoal : 1,
+//     goals : 0,
+//     cornerKicks : 0,
+//     goalKicks : 1,
+//     penaltyKicks : 1,
+//     throwIns : 0,
+//     appearances : 1
+//   },
+//   {
+//     jnumber : 101,
+//     name : 'Yohann Smot',
+//     foul : 0,
+//     redCard : 1,
+//     yellowCard : 1,
+//     shotsOnGoal : 1,
+//     goals : 0,
+//     cornerKicks : 0,
+//     goalKicks : 1,
+//     penaltyKicks : 1,
+//     throwIns : 0,
+//     appearances : 1
+//   }
+// ];
 
 function getStats(){
-  playerStats.forEach(function(player){
-    let playerNamesTemplate = document.getElementById('playerNamesTemplate').content.cloneNode(true);
-    let playerStatsTemplate = document.getElementById('playerStatsTemplate').content.cloneNode(true);
-    playerNamesTemplate.querySelector('.jnumber').textContent = '#' + player.jnumber;
-    playerNamesTemplate.querySelector('.name').textContent = player.name;
-    document.getElementById('playerNames').appendChild(playerNamesTemplate);
-    playerStatsTemplate.querySelector('.foul').textContent = player.foul;
-    playerStatsTemplate.querySelector('.redCard').textContent = player.redCard;
-    playerStatsTemplate.querySelector('.yellowCard').textContent = player.yellowCard;
-    playerStatsTemplate.querySelector('.shotsOnGoal').textContent = player.shotsOnGoal;
-    playerStatsTemplate.querySelector('.goals').textContent = player.goals;
-    playerStatsTemplate.querySelector('.cornerKicks').textContent = player.cornerKicks;
-    playerStatsTemplate.querySelector('.goalKicks').textContent = player.goalKicks;
-    playerStatsTemplate.querySelector('.penaltyKicks').textContent = player.penaltyKicks;
-    playerStatsTemplate.querySelector('.throwIns').textContent = player.throwIns;
-    playerStatsTemplate.querySelector('.appearances').textContent = player.appearances;
-    document.getElementById('playerStats').appendChild(playerStatsTemplate);
-  });
+   firebase.database().ref('stats').once('value')
+   .then(function(snapshot){
+     snapshot.forEach(function(player){
+       let playerNamesTemplate = document.getElementById('playerNamesTemplate').content.cloneNode(true);
+       let playerStatsTemplate = document.getElementById('playerStatsTemplate').content.cloneNode(true);
+       playerNamesTemplate.querySelector('.jnumber').textContent = '#' + player.val().jnumber;
+       playerNamesTemplate.querySelector('.name').textContent = player.val().name;
+       document.getElementById('playerNames').appendChild(playerNamesTemplate);
+       playerStatsTemplate.querySelector('.foul').textContent = player.val().foul;
+       playerStatsTemplate.querySelector('.redCard').textContent = player.val().redCard;
+       playerStatsTemplate.querySelector('.yellowCard').textContent = player.val().yellowCard;
+       playerStatsTemplate.querySelector('.shotsOnGoal').textContent = player.val().shotsOnGoal;
+       playerStatsTemplate.querySelector('.goals').textContent = player.val().goals;
+       playerStatsTemplate.querySelector('.cornerKicks').textContent = player.val().cornerKicks;
+       playerStatsTemplate.querySelector('.goalKicks').textContent = player.val().goalKicks;
+       playerStatsTemplate.querySelector('.penaltyKicks').textContent = player.val().penaltyKicks;
+       playerStatsTemplate.querySelector('.throwIns').textContent = player.val().throwIns;
+       playerStatsTemplate.querySelector('.appearances').textContent = player.val().appearances;
+       document.getElementById('playerStats').appendChild(playerStatsTemplate);
+     });
+   });
+  // playerStats.forEach(function(player){
+  //   let playerNamesTemplate = document.getElementById('playerNamesTemplate').content.cloneNode(true);
+  //   let playerStatsTemplate = document.getElementById('playerStatsTemplate').content.cloneNode(true);
+  //   playerNamesTemplate.querySelector('.jnumber').textContent = '#' + player.jnumber;
+  //   playerNamesTemplate.querySelector('.name').textContent = player.name;
+  //   document.getElementById('playerNames').appendChild(playerNamesTemplate);
+  //   playerStatsTemplate.querySelector('.foul').textContent = player.foul;
+  //   playerStatsTemplate.querySelector('.redCard').textContent = player.redCard;
+  //   playerStatsTemplate.querySelector('.yellowCard').textContent = player.yellowCard;
+  //   playerStatsTemplate.querySelector('.shotsOnGoal').textContent = player.shotsOnGoal;
+  //   playerStatsTemplate.querySelector('.goals').textContent = player.goals;
+  //   playerStatsTemplate.querySelector('.cornerKicks').textContent = player.cornerKicks;
+  //   playerStatsTemplate.querySelector('.goalKicks').textContent = player.goalKicks;
+  //   playerStatsTemplate.querySelector('.penaltyKicks').textContent = player.penaltyKicks;
+  //   playerStatsTemplate.querySelector('.throwIns').textContent = player.throwIns;
+  //   playerStatsTemplate.querySelector('.appearances').textContent = player.appearances;
+  //   document.getElementById('playerStats').appendChild(playerStatsTemplate);
+  // });
 }
 
 window.onload = () => {
